f188f48149340dc7030447cdcc2432274e54d982
C <chrismatusemail@gmail.com>
2020-11-08 14:34:56 +0000

    Initial CSS modules structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./css/App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`${styles.App}`}>
+      <header className={`${styles.AppHeader}`}>
+        <h1>LyricLab</h1>
       </header>
     </div>
   );
